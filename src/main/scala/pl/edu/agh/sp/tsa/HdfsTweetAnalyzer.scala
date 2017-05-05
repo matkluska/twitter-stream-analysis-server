5d03d658088367679bc5d9df9308fff38de55517
Mateusz Nowak <mateusznowak@vp.pl>
2017-05-05 22:40:20 +0200

    Some refactor, work in progress

--- src/main/scala/pl/edu/agh/sp/tsa/HdfsTweetAnalyzer.scala
+++ src/main/scala/pl/edu/agh/sp/tsa/HdfsTweetAnalyzer.scala
@@ -1,17 +1,24 @@
 package pl.edu.agh.sp.tsa
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import pl.edu.agh.sp.tsa.model.Tweet
 import spark.{Request, Response, Route, Spark}
 
+import scala.collection.mutable
 import scala.util.parsing.json.{JSONArray, JSONFormat, JSONObject}
 
 object HdfsTweetAnalyzer {
 
-  val sparkSession: SparkSession = SparkSession
+  private val sparkSession: SparkSession = SparkSession
     .builder()
     .master("local")
     .getOrCreate()
+
+  private val dataFrame: DataFrame = sparkSession
+    .sqlContext
+    .read
+    .json("hdfs://0.0.0.0:9000/user/flume/tweets")
 
   def main(args: Array[String]): Unit = {
     Spark.get("/", new Route {
@@ -20,35 +27,34 @@ object HdfsTweetAnalyzer {
           .getOrElse(Array[String]())
           .toList
 
+        val numberOfTweets: Int = Option(request.queryParams("numberOfTweets"))
+          .getOrElse("10")
+          .toInt
+
         response.header("Access-Control-Allow-Origin", "*")
 
-        val tweets: List[JSONObject] = getTopTenTweets(hashtags).map(_.toJSONObject)
+        val tweets: List[JSONObject] = getTopTweets(hashtags, numberOfTweets).map(_.toJSONObject)
         JSONArray.apply(tweets).toString(JSONFormat.defaultFormatter)
       }
     })
-
-    sparkSession
-      .sqlContext
-      .read
-      .json("hdfs://0.0.0.0:9000/user/flume/tweets")
-      .createOrReplaceTempView("tweets")
   }
 
-  def getTopTenTweets(hashtags: List[String]): List[Tweet] = {
-    println(hashtags)
-
-    sparkSession
-      .sqlContext
-      .sql("" +
-        "SELECT text " +
-        "FROM tweets " +
-        "ORDER BY quoted_status.retweet_count DESC " +
-        "LIMIT 10"
+  def getTopTweets(hashtags: List[String], numberOfTweets: Int): List[Tweet] = {
+    dataFrame
+      .select(
+        "text",
+        "quoted_status.retweet_count",
+        "entities.hashtags"
       )
+      //.filter() TODO filter against given hashtags
+      .orderBy(desc("retweet_count"))
+      .limit(numberOfTweets)
       .collect()
       .map(row => {
         val text = row.getString(0)
-        new Tweet(text)
+        val retweetCount = row.getLong(1)
+        val hashtags = row.getAs[mutable.WrappedArray[String]](2).toList
+        new Tweet(text, retweetCount, hashtags)
       })
       .toList
   }
--- src/main/scala/pl/edu/agh/sp/tsa/model/Tweet.scala
+++ src/main/scala/pl/edu/agh/sp/tsa/model/Tweet.scala
@@ -1,12 +1,14 @@
 package pl.edu.agh.sp.tsa.model
 
-import scala.util.parsing.json.JSONObject
+import scala.util.parsing.json.{JSONArray, JSONObject}
 
-class Tweet(var text: String) {
+class Tweet(var text: String, var retweetCount: Long, var hashtags: List[String]) {
 
   def toJSONObject: JSONObject = {
     val map: Map[String, Any] = Map(
-      "text" -> text
+      "text" -> text,
+      "retweetCount" -> retweetCount
+//      "hashtags" -> JSONArray.apply(hashtags)
     )
 
     JSONObject.apply(map)
